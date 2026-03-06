eeae21e0782e72585f0a1c4a9c7d7c78ba5a3bb6
Amanda Song <amandasong@uchicago.edu>
2026-03-05 23:53:45 -0600

    Cleaned file a little

--- src/get_data.py
+++ src/get_data.py
@@ -2,29 +2,30 @@ import httpx
 import csv
 import os
 import time
-from pathlib import Path
 from datetime import datetime
 
-REQUEST_DELAY = 0.1
+from datatypes import SF_EVICTIONS
+
+EVICTIONS_URL = "https://data.sfgov.org/resource/5cei-gny5.json"
+REQUEST_DELAY = 1
 
 
 def get_evictions_data() -> list[tuple]:
     """
-    Provides coordinates and dates of evictions in San Francisco
+    Retrieve coordinates and dates of evictions in San Francisco from 2020-2024.
 
     Returns:
-        List of tuples (id, lat, lon, YYYY-MM)
+        List of tuples (id, lat, lon, YYYY-MM) with coordinates and dates of evictions.
     """
-    url = "https://data.sfgov.org/resource/5cei-gny5.json"
-
     time.sleep(REQUEST_DELAY)
     params = {"$limit": 50000}
 
-    resp = httpx.get(url, params=params)
+    resp = httpx.get(EVICTIONS_URL, params=params)
     datas = resp.json()
 
-    eviction_list = []
+    evictions_list = []
     current_id = 1
+
     for data in datas:
         location = data.get("client_location")
         date_raw = data.get("file_date")
@@ -33,14 +34,13 @@ def get_evictions_data() -> list[tuple]:
             date_obj = datetime.strptime(date_raw, "%Y-%m-%dT%H:%M:%S.%f")
             year = date_obj.year
 
-            # filter years(2020-2024)
+            # Filter for years of interest (2020-2024)
             if 2020 <= year <= 2024:
-                #  check if in the data
+                # Check if data has latitude and longitude
                 lat = location.get("latitude")
                 lon = location.get("longitude")
 
                 if lat and lon:
-                    # save as tuple
                     date = date_obj.strftime("%Y-%m")
                     record = {
                         "id": int(current_id),
@@ -48,22 +48,22 @@ def get_evictions_data() -> list[tuple]:
                         "lon": float(lon),
                         "year_mon": date,
                     }
-                eviction_list.append(record)
+                evictions_list.append(record)
                 current_id += 1
 
-    return eviction_list
+    return evictions_list
 
 
-def save_evictions_to_csv(data_list, filename="clean-data/evictions_api_data.csv"):
+def save_evictions_to_csv(evictions_list):
     """
-    Save data from evictions API as a csv file
+    Save data from evictions API as a CSV file
     """
-    os.makedirs(os.path.dirname(Path(__file__).parent.parent / filename), exist_ok=True)
+    os.makedirs(os.path.dirname(SF_EVICTIONS), exist_ok=True)
     fieldnames = ["id", "lat", "lon", "year_mon"]
-    with open(filename, "w", newline="") as f:
+    with open(SF_EVICTIONS, "w", newline="") as f:
         csv_writer = csv.DictWriter(f, fieldnames=fieldnames)
         csv_writer.writeheader()
-        csv_writer.writerows(data_list)
+        csv_writer.writerows(evictions_list)
 
 
 if __name__ == "__main__":
