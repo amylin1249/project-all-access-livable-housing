66787dec00e30ebd060c914e18d2fca8c0ee2635
Amanda Song <amandasong@uchicago.edu>
2026-03-07 18:33:14 -0600

    Cleaned top of visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -4,89 +4,12 @@ import altair as alt
 import seaborn as sns
 import matplotlib.pyplot as plt
 from pathlib import Path
-from pyproj import Transformer, CRS
-from shapely import geometry
-from shapely.ops import transform
-from dash import Dash, html
-import dash_vega_components as dvc
+
 
 from .datatypes import (
     MERGED_SF_TRACTS_SHP,
     MERGED
 )
-
-
-# def visualize_sf_tracts():
-#     """
-#     Visualize SF tracts using MatPlotLib
-#     """
-#     sf_tracts = gpd.read_file(MERGED_SF_TRACTS_SHP)
-#     sf_tracts.plot()
-#     plt.show()
-
-
-# def load_shapefile(
-#     filename: str,
-# ) -> list[tuple]:
-#     """
-#     Load a shapefile using pyshp, returning Shapely geometries.
-#     """
-#     # this will be a list of (geometry, feature_dict) tuples
-#     geometries = []
-
-#     # opens the file (expects a file that ends in .shp)
-#     sf = shapefile.Reader(filename, encodingErrors="replace")
-
-#     fields = [field[0] for field in sf.fields[1:]]
-
-#     for record in sf.shapeRecords():
-#         # a `.shape.__geo_interface__` (essentially a list of points)
-#         # This is passed to the `shapely.shape` function to create
-#         # an instance of the appropriate geometry class.
-#         geom = geometry.shape(record.shape.__geo_interface__)
-
-#         # these objects also have a `.record` attribute
-#         # which is a list-like object with all attributes
-#         # we'll keep the shape in a tuple with its record data
-#         geometries.append((geom, dict(zip(fields, record.record))))
-
-#     return geometries
-
-
-# def quick_map(shapefile_data: list[tuple]):
-#     """
-#     Given shapefile data (returned from load_shapefile)
-#     create a folium map with all geometries for quick visualization.
-#     """
-#     centroid = shapefile_data[0][0].centroid
-#     map = folium.Map(location=[centroid.y, centroid.x], zoom_start=6)
-#     for geom, feat in shapefile_data:
-#         folium.GeoJson(geom.__geo_interface__).add_to(map)
-#     map.save("map.html")
-#     print("created map.html, trying to open browser...")
-#     webbrowser.open("file://" + str(Path.cwd() / "map.html"))
-
-
-# def reproject_geometries(
-#     shapefile_data: list[tuple], from_epsg: str, to_epsg: str
-# ) -> list[tuple]:
-#     """Reproject geometries from one CRS to another."""
-#     transformer = Transformer.from_crs(from_epsg, to_epsg, always_xy=True)
-
-#     reprojected = [
-#         (transform(transformer.transform, geom), feat) for geom, feat in shapefile_data
-#     ]
-#     return reprojected
-
-
-# def get_epsg_from_file(filename: str):
-#     """
-#     This function should read in the text from the PRJ file parameter
-#     and uses
-#     """
-#     with open(filename, "r") as f:
-#         prj_text = f.read()
-#     return CRS.from_wkt(prj_text).to_epsg()
 
 
 def create_tract_map(
